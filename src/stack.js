53f4300957d0cdff51ae716f75c7bf9ea4e32fff
Kirill Gudeta <kirillgudeta@gmail.com>
2025-01-06 16:56:55 +0300

    init: 4 pass

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor() {
+		this.stack = []
+	}
+	push(element) {
+		this.stack.push(element)
+	}
+	pop() {
+		return this.stack.pop()
+	}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	peek() {
+		const element = this.stack.pop()
+		this.stack.push(element)
+		return element
+	}
 }
 
 module.exports = {
-  Stack
-};
+	Stack,
+}
