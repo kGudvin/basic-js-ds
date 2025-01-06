60779decbd7c440051c7f47f142022169e0d576e
Kirill Gudeta <kirillgudeta@gmail.com>
2025-01-06 17:35:19 +0300

    feat: 10 done

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,51 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
-// const { Node } = require('../extensions/list-tree.js');
+const { ListNode } = require('../extensions/list-node.js')
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+	constructor() {
+		this.head = null
+		this.tail = null
+	}
+	root() {
+		return this.head
+	}
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	add(data) {
+		throw new NotImplementedError('Not implemented')
+		// remove line with error and write your code here
+	}
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	has(/* data */) {
+		throw new NotImplementedError('Not implemented')
+		// remove line with error and write your code here
+	}
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	find(/* data */) {
+		throw new NotImplementedError('Not implemented')
+		// remove line with error and write your code here
+	}
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	remove(/* data */) {
+		throw new NotImplementedError('Not implemented')
+		// remove line with error and write your code here
+	}
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	min() {
+		throw new NotImplementedError('Not implemented')
+		// remove line with error and write your code here
+	}
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	max() {
+		throw new NotImplementedError('Not implemented')
+		// remove line with error and write your code here
+	}
 }
 
 module.exports = {
-  BinarySearchTree
-};
+	BinarySearchTree,
+}
--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js')
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,23 +14,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+	constructor() {
+		this.head = null
+		this.tail = null
+	}
+	getUnderlyingList() {
+		return this.head
+	}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	enqueue(value) {
+		const newNode = new ListNode(value)
+		if (this.tail) {
+			this.tail.next = newNode
+		}
+		this.tail = newNode
+		if (!this.head) {
+			this.head = newNode
+		}
+	}
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	dequeue() {
+		if (!this.head) {
+			return null
+		}
+		const removedValue = this.head.value
+		this.head = this.head.next
+		if (!this.head) {
+			this.tail = null
+		}
+		return removedValue
+	}
 }
 
 module.exports = {
-  Queue
-};
+	Queue,
+}
